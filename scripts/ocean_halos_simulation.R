dff97d6c3e065d4d0b60f3362bfa0e6b347fa877
jcvdav <juancarlos.villader@gmail.com>
2019-12-05 14:56:39 -0800

    Added effort heat maps

--- scripts/ocean_halos_simulation.R
+++ scripts/ocean_halos_simulation.R
@@ -71,13 +71,166 @@ ggplot(res, aes(x = L, y = w, fill = equil_b, z = equil_b)) +
 
 
 # Effort heat maps here
+w <- 180e2
+chis <- seq(0, 30000, by = 500)
+Ls <- seq(0.1, 1, by = 0.05)
+alphas <- seq(alpha / 10, alpha * 10, length.out = 50)
+
+
+E_chi_L <- expand_grid(L = Ls, chi = chis) %>% 
+  mutate(equil_E_f = pmap_dbl(.l = list(L = L, chi = chi),
+                              .f = wrapper,
+                              r = r,
+                              K = K,
+                              X0 = X0,
+                              f = f,
+                              p = p,
+                              q = q,
+                              c = c,
+                              beta = beta,
+                              alpha = alpha,
+                              mu = mu,
+                              w = w,
+                              years = years,
+                              want = "E_f_vec"),
+         equil_E_l = pmap_dbl(.l = list(L = L, chi = chi),
+                              .f = wrapper,
+                              r = r,
+                              K = K,
+                              X0 = X0,
+                              f = f,
+                              p = p,
+                              q = q,
+                              c = c,
+                              beta = beta,
+                              alpha = alpha,
+                              mu = mu,
+                              w = w,
+                              years = years,
+                              want = "E_l_vec"),
+         equil_E_i = pmap_dbl(.l = list(L = L, chi = chi),
+                              .f = wrapper,
+                              r = r,
+                              K = K,
+                              X0 = X0,
+                              f = f,
+                              p = p,
+                              q = q,
+                              c = c,
+                              beta = beta,
+                              alpha = alpha,
+                              mu = mu,
+                              w = w,
+                              years = years,
+                              want = "E_i_vec")) %>% 
+  gather(patch, effort, -c(L, chi)) %>% 
+  group_by(patch) %>% 
+  mutate(max_e = max(effort)) %>% 
+  ungroup() %>% 
+  mutate(effort_norm = effort / max_e,
+         patch = case_when(patch == "equil_E_f" ~ "Fishing zone",
+                           patch == "equil_E_l" ~ "Lease zone",
+                           T ~ "No-take zone")
+  )
+
+ggplot(data = E_chi_L,
+       mapping = aes(x = L, y = chi, fill = effort_norm, z = effort_norm)) +
+  geom_raster(interpolate = T) +
+  facet_wrap(~patch, ncol = 1) +
+  scale_x_continuous(breaks = seq(0, 1, by = 0.1), expand = c(0, 0)) +
+  scale_y_continuous(expand = c(0, 0)) +
+  scale_fill_viridis_c() +
+  guides(fill = guide_colorbar(title = "Normalized\neffort",
+                               frame.colour = "black",
+                               ticks.colour = "black")) +
+  startR::ggtheme_plot() +
+  labs(x = "Proportion of reserve\nas lease zone (L)", y = quote("Lease price ("~chi~")")) +
+  ggtitle(label = "Equilibrium effort in each zone")
 
 
 
+# E_chi_L %>% 
+#   filter(L %in% c(0.1, 0.5, 0.9)) %>% 
+#   mutate(dist = case_when(patch == "Fishing zone" ~ 1.5 * f,
+#                            patch == "Lease zone" ~ f * (1 - L) + (f * L / 2),
+#                            T ~ 0.5 * f * (1 - L)),
+#          L = paste("L = ", L)) %>% 
+#   ggplot(mapping = aes(x = dist, y = effort, color = chi, group = chi)) +
+#   geom_line() +
+#   facet_wrap(~L, ncol = 1) + 
+#   theme_minimal()
 
+# alpha, L, and efforts
 
+E_alpha_L <- expand_grid(L = Ls, alpha = alphas) %>% 
+  mutate(equil_E_f = pmap_dbl(.l = list(L = L, alpha = alpha),
+                              .f = wrapper,
+                              r = r,
+                              K = K,
+                              X0 = X0,
+                              f = f,
+                              p = p,
+                              q = q,
+                              c = c,
+                              beta = beta,
+                              chi = chi,
+                              mu = mu,
+                              w = w,
+                              years = years,
+                              want = "E_f_vec"),
+         equil_E_l = pmap_dbl(.l = list(L = L, alpha = alpha),
+                              .f = wrapper,
+                              r = r,
+                              K = K,
+                              X0 = X0,
+                              f = f,
+                              p = p,
+                              q = q,
+                              c = c,
+                              beta = beta,
+                              chi = chi,
+                              mu = mu,
+                              w = w,
+                              years = years,
+                              want = "E_l_vec"),
+         equil_E_i = pmap_dbl(.l = list(L = L, alpha = alpha),
+                              .f = wrapper,
+                              r = r,
+                              K = K,
+                              X0 = X0,
+                              f = f,
+                              p = p,
+                              q = q,
+                              c = c,
+                              beta = beta,
+                              chi = chi,
+                              mu = mu,
+                              w = w,
+                              years = years,
+                              want = "E_i_vec")) %>% 
+  gather(patch, effort, -c(L, alpha)) %>% 
+  group_by(patch) %>% 
+  mutate(max_e = max(effort)) %>% 
+  ungroup() %>% 
+  mutate(effort_norm = effort / max_e,
+         patch = case_when(patch == "equil_E_f" ~ "Fishing zone",
+                           patch == "equil_E_l" ~ "Lease zone",
+                           T ~ "No-take zone")
+  )
 
-
+ggplot(data = E_alpha_L,
+       mapping = aes(x = L, y = alpha, fill = effort_norm, z = effort_norm)) +
+  geom_raster(interpolate = T) +
+  facet_wrap(~patch, ncol = 1) +
+  scale_x_continuous(breaks = seq(0, 1, by = 0.1), expand = c(0, 0)) +
+  scale_y_continuous(expand = c(0, 0)) +
+  scale_fill_viridis_c() +
+  guides(fill = guide_colorbar(title = "Normalized\neffort",
+                               frame.colour = "black",
+                               ticks.colour = "black")) +
+  startR::ggtheme_plot() +
+  labs(x = "Proportion of reserve\nas lease zone (L)", y = quote("Cost of enforcement("~alpha~")")) +
+  ggtitle(label = "Equilibrium effort in each zone")
 
 # SCATTER PLOTS (lines, or whatever)
 
