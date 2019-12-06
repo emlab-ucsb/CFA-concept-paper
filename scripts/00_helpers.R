d9dafc6040dfcd7cef776dc63b98a151e35596ef
jcvdav <juancarlos.villader@gmail.com>
2019-12-06 10:30:52 -0800

    Added helper functions, moved plot theme function to helpers

--- scripts/00_helpers.R
+++ scripts/00_helpers.R
@@ -1,3 +1,108 @@
+########################################################
+#                    HELPER FUNCTIONS                  #
+########################################################
+# This script contains some helper functions to 
+# streamline our work.
+########################################################
+
+#### MODEL STUFF ####
+# The function below returns a panel figure
+# with all state variables through time
+fast_check <- function(r, K, X0, f, p, q, c, beta, L, alpha, mu, w, chi, years) {
+  model(r, K, X0, f, p, q, c, beta, L, alpha, mu, w, chi, years) %>% 
+    gather(variable, value, - time) %>% 
+    ggplot(aes(x = time, y = value)) +
+    geom_line() +
+    facet_wrap(~variable, scale = "free_y")
+}
+
+
+
+#### FIGURE STUFF ####
+# Plot theme function
+
+text_size <- 10
+title_size <- 12
+
+plot_theme <- theme_bw() +
+  theme(text = element_text(size = text_size),
+        plot.title = element_text(size = title_size),
+        rect = element_rect(fill = "transparent",
+                            colour = NA,
+                            color = NA,
+                            size = 0,
+                            linetype = 0),
+        legend.background = element_blank(),
+        legend.key = element_blank(),
+        strip.background = element_blank())
+
+
+
+
+plot_theme <- function (font_size = 10, font_family = "", line_size = 0.5) {
+  
+  half_line <- font_size / 2
+  
+  theme_grey(base_size = font_size, base_family = font_family) %+replace%
+    theme(
+      rect = element_rect(
+        fill = "transparent",
+        colour = NA,
+        color = NA,
+        size = 0,
+        linetype = 0),
+      text = element_text(
+        family = font_family,
+        face = "plain",
+        colour = "black",
+        size = font_size,
+        hjust = 0.5,
+        vjust = 0.5,
+        angle = 0,
+        lineheight = 0.9,
+        margin = margin(),
+        debug = FALSE),
+      plot.title = element_text(
+        family = font_family,
+        face = "plain",
+        colour = "black",
+        size = rel(1.2),
+        margin = margin(),
+        debug = FALSE),
+      axis.text = element_text(
+        colour = "black",
+        size = rel(0.8)),
+      axis.ticks = element_line(
+        colour = "black"),
+      axis.line = element_line(
+        colour = "black",
+        size = line_size,
+        lineend = "square"),
+      axis.line.x = element_line(
+        colour = "black",
+        size = line_size,
+        lineend = "square"),
+      axis.line.y = element_line(
+        colour = "black",
+        size = line_size,
+        lineend = "square"),
+      legend.background = element_blank(),
+      legend.key = element_blank(),
+      legend.key.size = unit(1, "lines"),
+      legend.spacing = unit(0.4, "cm"),
+      legend.text = element_text(size = rel(0.8)),
+      panel.background = element_blank(),
+      strip.background = element_blank(),
+      plot.background = element_blank(),
+      complete = TRUE
+    )
+}
+
+
+
+
+# Save function
+# The function saves figures as png and pdf
 lazy_ggsave <- function(plot, filename, width = 7, height = 5){
   # Save as png
   ggsave(plot = plot,
