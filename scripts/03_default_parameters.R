dacc93241fe1e8cd612e0abf46c7d3c85e82a4f5
jcvdav <juancarlos.villader@gmail.com>
2019-12-10 11:51:22 -0800

    Updated default parameters to fix #2

--- scripts/03_default_parameters.R
+++ scripts/03_default_parameters.R
@@ -4,6 +4,7 @@
 # and we may have a source.
 #
 ########################################################################
+
 ## Bio
 r <- 0.57                 # *Growth rate
 K <- 6.876526e6           # *Carrying capacity (tons)
@@ -11,20 +12,20 @@ X0 <- 3.507028e6          # *Initial biomass (tons)
 D <- matrix(c(0.7, 0.3,
               0.3, 0.7),
             nrow = 2,
-            byrow = T)                 # *Dispersal between patches
+            byrow = T)    # *Dispersal between patches
 
 ## Econ
-p <- 1200                 # *Price (USD / ton)
-q <- 1.5e-5               # *Catchability (made up)
+p <- 1100                 # *Price (USD / ton)
+q <- 2e-5                 # *Catchability (made up)
 c <- 1800                 # *Cost coefficient (USD / unit of effort)
 beta <- 1.3               # *Cost exponent (From upsides?)
 
 ## Management
 L <- 0.7                  # Size of the lease zone
-alpha <- 9000              # Marginal cost of enforcement
+alpha <- 9000             # Marginal cost of enforcement
 mu <- 1e-3                # Enforcement coefficient
-w <- 9000                # Per-unit-effort fine
-chi <- 1000               # Per-unit-access fee
+w <- 5 * c                # Per-unit-effort fine
+chi <- 10000              # Per-unit-access fee
 
 ## Duration of simulation
 years <- 30L              # *Duration of simulaionS
