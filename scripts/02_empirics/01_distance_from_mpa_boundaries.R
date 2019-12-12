93055d2e5e34e9ab53914a9837e7920ed62f9862
jcvdav <juancarlos.villader@gmail.com>
2019-12-12 12:19:17 -0800

    Added code to calculate distance to and from LSMPA boundaries

---
+++ scripts/02_empirics/01_distance_from_mpa_boundaries.R
@@ -0,0 +1,265 @@
+####################################################################################
+#####                       distance_from_mpa_boundaries                        ####
+####################################################################################
+#
+# This script will create a gridded dataset of "distance to MPA boundary".
+# We will use all MPAs with more than 10,000 km2 extension, and 
+# with some portion designated as no-take.
+# 
+# The final dataset will have the following columns:
+#   - lat:        center of the latitude bin
+#   - lon:        center of the longitude bin
+#   - distance:   negative (positive) values are inside (outside) the border
+#   - wdpa_pid:   polygon identifier in WDPA
+#   - no_take:    dummy variable indicating if ALL the MPA is no-take
+# 
+# This dataset will be used to categorize fishing effort around MPA boundaries.
+####################################################################################
+
+
+
+## SET UP SECTION ##################################################################
+## Load libraries
+library(here)
+library(sf)
+library(bigrquery)
+library(lwgeom)
+library(raster)
+library(fasterize)
+library(janitor)
+library(tidyverse)
+
+## Load helper functions
+source(here("scripts", "00_helpers.R"))
+
+## Load data
+# We beggin by loading the MPA boundaries, available from protectedplanet.net
+mpa_boundaries <-
+  st_read(here::here("data", "WDPA_Nov2019_No_Terrestrial/"),
+          layer = "WDPA_Nov2019_No_Terrestrial",
+          stringsAsFactors = FALSE,
+          as_tibble = TRUE) %>%
+  clean_names(case = "snake") %>%                              # Clean column names
+  select(wdpa_pid,                                             # Select features
+                area_km = rep_m_area,
+                no_take,
+                year = status_yr) %>%
+  st_make_valid() %>%                                          # Fix weird polygons
+  st_cast("MULTIPOLYGON")                                      # Cast to multipolygon
+
+
+
+## PROCESS SECTION ####################################################################
+## Filter MPAS we want
+# There are polygons we don't want, like duplicated polygons or the ross sea area
+wdpa_pid_discard <- c("555624810_A",
+                      "555624810_D",
+                      "555624810_E",
+                      "555556875_A")
+
+
+# Filter for only MPAs that include some portion as a no-take and filter by siz
+no_take_lsmpa_boundaries <-
+  mpa_boundaries %>%
+  filter(no_take %in% c("All", "Part")) %>%                   # Keep only MPAS with a no-take portion
+  filter(!wdpa_pid %in% wdpa_pid_discard) %>%                 # Remove duplicates
+  filter(area_km > 100000) %>%                                # Keep areas larger than 10,000 Km2
+  filter(year != 0) %>%                                       # Remove ones with missing year
+  st_transform(54009) %>%                                     # Reproject to Mollweide (https://epsg.io/54009)
+  nngeo::st_remove_holes() %>%                                # Keep the boundary only (i.e. remove interior borders)
+  mutate(wdpa_pid_num = str_extract(wdpa_pid, "[:digit:]*"),  # Extract the numeric characters only
+         wdpa_pid_num = as.numeric(wdpa_pid_num))             # Transform to numeric
+
+lonlat_crs <- "+proj=longlat +datum=WGS84 +no_defs"           # longlat crs proj4string
+mol_crs <- st_crs(no_take_lsmpa_boundaries)$proj4string       # Extract the mollweide crs proj4string
+
+# Visual check #1
+plot(no_take_lsmpa_boundaries, max.plot = 1)
+
+## Rassterization
+# The first step is to create a reference raster that covers the whole world
+r <- raster(xmn = -180, xmx = 180, ymn = -90, ymx = 90,
+            res = 0.1,                                        # Set resolution
+            val = 1,                                          # Assign a value
+            crs = lonlat_crs) %>%                           # Unprojected coords
+  projectRaster(crs = mol_crs)
+
+# We will have two raster:
+# - One with the identity of each polygon
+# - One with the distance to each boundary
+# First ,the identity one
+wdpa_pid_raster <- 
+  no_take_lsmpa_boundaries %>% 
+  st_buffer(100 * 1e3 * 1.854) %>%                             # Create a 100 nm
+  st_cast("MULTIPOLYGON") %>%                                  # Cast
+  fasterize(r, field = "wdpa_pid_num") %>%                     # Rasterize
+  projectRaster(crs = lonlat_crs,
+                res = 0.1)                                     # Reproject to longlat (whan we need for GFW)
+
+# Visual check #2
+plot(wdpa_pid_raster)
+
+
+# Now, we'll create the distance to border raster.
+# This will be composed of two rasters:
+# - Distance from outside to the border
+# - Distance from inside to the border
+outside_raster <- fasterize(no_take_lsmpa_boundaries, r)        # Rasterize the boundaries
+distance_outside <- distance(outside_raster)                    # Calculate the distance to the boundaries
+
+# Visual check #3
+plot(distance_outside)
+
+# Now we repeat the process but calculate distance form inside (negative numbers)
+inside_raster <- is.na(outside_raster)                          # A raster with 1 and 0 for outside / inside
+inside_raster[inside_raster == 0] <- NA                         # Replace 0 (inside) with NAs to be filled in
+distance_inside <- - distance(inside_raster)                    # Calculate distance to border (note the "-")
+
+# Visual check #4
+plot(distance_inside)
+
+# We'll now combine both
+distance_raster <- distance_outside
+distance_raster[distance_inside < 0] <- distance_inside[distance_inside < 0]
+distance_final <- projectRaster(distance_raster, crs = lonlat_crs, res = 0.1)
+distance_final[distance_final > 100 * 1e3 * 1.854] <- NA
+
+# Visual check #5
+plot(distance_final)
+
+## TABULIZE ################################################################################
+# The next step is to make the rasters into a table that can be uploaded to GBQ
+# wdpa_pid first
+wdpa_pid_table <- as.data.frame(wdpa_pid_raster, xy = T) %>% 
+  rename(lon = x, lat = y, wdpa_pid_num = layer)
+
+# Now distance
+distance_final_table <- as.data.frame(distance_final, xy = T) %>% 
+  rename(lon = x, lat = y, distance = layer)
+
+# Cobmine them
+output_table <- left_join(distance_final_table, wdpa_pid_table, by = c("lon", "lat")) %>% 
+  mutate_at(vars(lon, lat), function(x){(floor(x / 0.1) * 0.1) + 0.05})
+
+# Export a nicer version as a map
+
+coast <- rnaturalearth::ne_countries(scale = "small", returnclass = "sf")
+
+distance_to_no_take_mpa_border_map <- 
+  output_table %>% 
+  drop_na(distance) %>% 
+  filter(distance > -100e3 * 1.854) %>% 
+  ggplot() +
+  geom_tile(aes(x = lon, y = lat, fill = distance)) +
+  geom_sf(data = st_transform(no_take_lsmpa_boundaries, lonlat_crs),
+          fill = "transparent",
+          color = "red") +
+  geom_sf(data = coast, color = "transparent", fill = "black") +
+  startR::ggtheme_map() +
+  scale_fill_gradient2(low = "red", high = "blue", mid = "white", midpoint = 0) +
+  guides(fill = guide_colorbar(title = "Distance",
+                               ticks.colour = "black",
+                               frame.colour = "black")) 
+  
+lazy_ggsave(plot = distance_to_no_take_mpa_border_map,
+            filename = "distance_to_no_take_mpa_border_map")
+
+# Export the data
+write.csv(output_table,
+          here("data", "gridded_distance_from_lsmpa_borders.csv"),
+          row.names = F)
+
+## SEND TO GOOGLE BIG QUERY ##############################################################
+ucsb_project <- "emlab-gcp" # Name of our project
+
+# Create a bq_table object
+dist_to_lsmpa <- 
+  bq_table(project = ucsb_project,
+           dataset = "ocean_halos_v2",
+           table = "dist_to_lsmpa") 
+
+# Overwrite or create
+if(!bq_table_exists(dist_to_lsmpa)){
+  bq_table_create(dist_to_lsmpa,
+                  fields = output_table) %>%
+    bq_table_upload(values = output_table)
+  
+} else {
+  bq_table_delete(bq_table(project = ucsb_project,
+                           dataset = "ocean_halos_v2",
+                           table = "dist_to_lsmpa"))
+  
+  bq_table_create(dist_to_lsmpa,
+                  fields = output_table) %>%
+    bq_table_upload(values = output_table)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# WITH
+# vessels_i_want AS (
+#   SELECT
+#   ssvid,
+#   best.best_vessel_class,
+#   best.best_engine_power_kw
+#   FROM
+#   `world-fishing-827.gfw_research.vi_ssvid_v20190430`
+#   WHERE
+#   best.best_vessel_class IN ("tuna_purse_seines",
+#                              "drifting_longlines",
+#                              "trawlers")
+#   AND on_fishing_list_best
+#   AND activity.active_hours > 0),
+# #
+# #
+# #
+# #
+# ########
+# gridded_effort_by_vessel_and_year AS (
+#   SELECT
+#   EXTRACT(year
+#           FROM
+#           timestamp) AS year,
+#   best_vessel_class,
+#   CAST(FLOOR(lat / 0.1) * 0.1 AS NUMERIC) + 0.05 AS lat,
+#   CAST(FLOOR(lon / 0.1) * 0.1 AS NUMERIC) + 0.05 AS lon,
+#   SUM(hours) AS fishing_hours
+#   FROM
+#   `world-fishing-827.gfw_research.pipe_production_v20190502_fishing`
+#   LEFT JOIN
+#   vessels_i_want
+#   USING
+#   (ssvid)
+#   WHERE
+#   ssvid IN (
+#     SELECT
+#     ssvid
+#     FROM
+#     `vessels_i_want`)
+#   AND nnet_score > 0.5
+#   AND distance_from_shore_m > 1000
+#   AND DATE(_PARTITIONTIME) > "2015-12-31"
+#   GROUP BY
+#   year,
+#   lat,
+#   lon,
+#   best_engine_power_kw,
+#   best_vessel_class
+#   LIMIT
+#   1000 )
+# 
+# SELECT * FROM gridded_effort_by_vessel_and_year 
