bb6f2bf327c3fd1ad551b7ff579c4929de708fcf
jcvdav <juancarlos.villader@gmail.com>
2019-12-18 22:02:18 -0500

    Updating night lights, which are not cool

--- scripts/02_empirics/05_fishing_the_line_with_night_lights.R
+++ scripts/02_empirics/05_fishing_the_line_with_night_lights.R
@@ -29,23 +29,34 @@ write.csv(x = night_lights_data,
           file = here("data", "gridded_night_lights_by_year_dist_to_mpa.csv"),
           row.names = F)
 
+increment <- 10
+
 night_lights_in_10k_increments <- night_lights_data %>%
   filter(between(distance, -100e3, 150e3)) %>%                        # Keep only data in a 100 Km buffer from the line
   filter(year > 2016) %>%
   mutate(dist = round(distance / (10 * 1e3)) * 10) %>%  # Mutate the distance to group by a common bin
-  group_by(year, dist) %>%                                            # Define grouping variables
+  filter(between(distance, -100e3, 150e3)) %>%                        # Keep only data in a 100 Km buffer from the line
+  filter(year > 2016) %>% 
+  filter(wdpaid %in% c(309888, 555629385, 400011,	220201, 11753)) %>% 
+  mutate(dist = round(distance / (increment * 1e3)) * increment) %>%  # Mutate the distance to group by a common bin
+  mutate(name = case_when(wdpaid == 309888 ~ "PIPA",
+                          wdpaid == 555629385 ~ "Revillagigedo",
+                          wdpaid == 400011 ~ "PRINMS",
+                          wdpaid == 220201 ~ "PNMS",
+                          wdpaid == 11753 ~ "Galapagos"),
+         name = fct_relevel(name, c("Galapagos", "Revillagigedo"), after = Inf)) %>% 
+  group_by(name, dist) %>%                                            # Define grouping variables
   summarize(rad = sum(sum_rad, na.rm = T)) %>%                        # Calculate total
-  ungroup() %>%
-  mutate(year = as.factor(year))
+  ungroup()
 
 
 fishing_the_line_night_lights_plot <- 
   ggplot(data = night_lights_in_10k_increments,
-         aes(x = dist, y = rad / 1e4, color = year)) +
+         aes(x = dist, y = rad / 1e4)) +
   geom_smooth(method = "loess", se = F, color = "black") +
-  geom_point(aes(fill = year), color = "black", shape = 21, size = 2) +
+  geom_point(fill = "red", color = "black", shape = 21, size = 2) +
+  facet_wrap(~name, scales = "free_y") +
   geom_vline(xintercept = 0, linetype = "dashed") +
-  scale_fill_brewer(palette = "Set1") +
   scale_y_continuous(limits = c(0, NA)) +
   plot_theme() +
   guides(fill = guide_legend(title = "Year")) +
@@ -55,3 +66,4 @@ fishing_the_line_night_lights_plot <-
 lazy_ggsave(plot = fishing_the_line_night_lights_plot,
             filename = "fishing_the_line_night_lights_plot",
             height = 2.5, width = 3.5)
+
