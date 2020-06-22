b21ade396a714f673dfff12a409b462c61a537c2
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2020-06-22 13:03:45 -0700

    Finalized script for figure with CFA areas

--- scripts/02_empirics/pna/04_get_gridded_effort_by_gear_and_year_dist_to_mpa.R
+++ scripts/02_empirics/pna/04_get_gridded_effort_by_gear_and_year_dist_to_mpa.R
@@ -34,4 +34,4 @@ data <- tbl(ocean_halos_v2, table_name) %>%
 
 # Save the data
 saveRDS(object = data,
-        file = here("data", paste0(table_name, ".rds")))
+        file = here("raw_data", paste0(table_name, ".rds")))
